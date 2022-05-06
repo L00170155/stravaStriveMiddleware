9d87379f30f2fc2535402049581a0e5cb98ddefe
L00170155 <40305076+brianmcnic@users.noreply.github.com>
2022-05-06 22:42:05 +0100

    strava auth

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const fact = require("./fact");
+const auth = require("./stravaauth");
 console.log('index')
 router.get("/facts", fact);
+router.get("/stravaauth", auth);
 module.exports = router;
